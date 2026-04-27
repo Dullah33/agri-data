37d8849aa30eb3a1eb9dc03aeaabd703da4470fa
Abdullah Abil <abilabiyoso1@student.uns.ac.id>
2026-04-27 09:39:38 +0700

    update config

--- api/config/koneksi.php
+++ api/config/koneksi.php
@@ -5,8 +5,23 @@ $user = "2PYtsc69VVAWGrB.root";
 $pass = "yEfXagDr03g7t2EL";
 $db   = "drive-agridata";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_init();
+
+// WAJIB untuk TiDB Cloud
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+?>
